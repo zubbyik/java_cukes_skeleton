a7f3cff3455ad1443ce54bf71ab621bf1173608a
Malachy Egbuna <zubbyik@gmail.com>
2021-04-23 23:34:30 +0100

    modified src/main, src/test

--- src/main/java/io/cucumber/skeleton/Belly.java
+++ src/main/java/io/cucumber/skeleton/Belly.java
@@ -1,7 +1,12 @@
 package io.cucumber.skeleton;
 
 public class Belly {
+    int belly_size;
     public void eat(int cukes) {
+        belly_size += cukes;
+    }
 
+    public int grow(){
+        return belly_size;
     }
 }
--- src/test/java/io/cucumber/skeleton/StepDefinitions.java
+++ src/test/java/io/cucumber/skeleton/StepDefinitions.java
@@ -1,11 +1,34 @@
 package io.cucumber.skeleton;
 
+import io.cucumber.java.Before;
 import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 public class StepDefinitions {
+    Belly belly;
+
+    @Before
+    public void setup(){
+        belly = new Belly();
+    }
+
+
+
     @Given("I have {int} cukes in my belly")
     public void I_have_cukes_in_my_belly(int cukes) {
-        Belly belly = new Belly();
         belly.eat(cukes);
     }
+
+    @When("I wait {int} hour")
+    public void i_wait_hour(Integer int1) throws InterruptedException {
+        // Write code here that turns the phrase above into concrete actions
+        Thread.sleep(int1);
+    }
+    @Then("my belly should growl")
+    public void my_belly_should_growl() {
+        // Write code here that turns the phrase above into concrete actions
+        System.out.println("This is how large my belly has grown after eating some cukes "+belly.grow());
+    }
+
 }
